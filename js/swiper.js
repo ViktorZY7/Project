9f24717e0cebf6c18c1db7f743d6bdd1e0b0c45c
Viktor Kuvayskov <viktorkuvayskov@gmail.com>
2017-09-08 08:55:16 +0300

    First commit

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $('.slider1').slick({
+    dots: true,
+  });
+});
+    
+
+$(document).ready(function(){
+  $('.slider2').slick({
+  dots: true,
+  slidesToShow: 4,
+  arrow: true
+  });
+});
+    
