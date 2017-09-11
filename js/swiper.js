07358ee3ef84ccc8345f0693b5f5bcca901f1a0a
Viktor Kuvayskov <viktorkuvayskov@gmail.com>
2017-09-11 10:33:56 +0300

    Redacting Fonts

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,41 @@
 
 $(document).ready(function(){
   $('.slider1').slick({
-    dots: true,
+    dots: false,
   });
 });
     
 
 $(document).ready(function(){
   $('.slider2').slick({
-  dots: true,
+  dots: false,
   slidesToShow: 4,
-  arrow: true
+  arrow: true,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
   });
 });
     
